e6ece3c128e895f13b7f3b2f67b391d9bc265977
unknown <asim26k@gmail.com>
2026-01-23 16:24:48 +0530

    Fix frontend being served on API routes too.

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,24 +4,27 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import connectDB from "./utils/db.js";
+import authRoute from "./routes/authRoute.js";
 
 const app = express();
+app.use(express.json());
+
+app.use("/api/v1/auth", authRoute);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const joinedPath = path.join(__dirname, "../frontend/dist/index.html");
-
-console.log("joinedPath : ", joinedPath);
-
 const PORT = process.env.PORT || 3030;
 
-// Making App ready for Deployment
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+// MAking app to serve Frontend in Production
 
-  app.use("*path", (req, res) => {
-    // In Express 5 '*' is not supported use this syntax instead '*name'
+console.log(process.env.NODE_ENV === "production");
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../frontend/dist/")));
+
+  // In Express 5 '*' is not supported use this syntax instead '*name'
+  app.use("*name", (req, res) => {
     res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
   });
 }
